5f37c738a631ccbc151b4ff137bf4f71f633fbd9
xqm32 <458173774@qq.com>
2023-06-29 20:10:42 +0800

    chore: remove all `Card`

--- lib/resident/index/index.dart
+++ lib/resident/index/index.dart
@@ -27,11 +27,11 @@ class _ResidentIndexState extends State<ResidentIndex> {
         child: Column(
           children: [
             ResidentIndexNotification(notifications: widget.notifications),
-            const SizedBox(height: 8),
+            const Divider(height: 8),
             ResidentIndexService(
               communityId: widget.communityId,
             ),
-            const SizedBox(height: 8),
+            const Divider(height: 8),
             ResidentIndexNews(notifications: widget.notifications),
           ],
         ),
@@ -51,23 +51,21 @@ class ResidentIndexNotification extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: const Icon(Icons.notifications),
-        title: Row(
-          children: [
-            Text(notifications.first.getStringValue('title')),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text('查看'),
-                ),
+    return ListTile(
+      leading: const Icon(Icons.notifications),
+      title: Row(
+        children: [
+          Text(notifications.first.getStringValue('title')),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text('查看'),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -84,70 +82,68 @@ class ResidentIndexService extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.person,
-                text: '实名认证',
-                color: Colors.orange,
-              ),
-              ResidentIndexServiceIcon(
-                onPressed: () => navPush(
-                    context, ResidentHouseList(communityId: communityId)),
-                icon: Icons.home,
-                text: '房屋管理',
-                color: Colors.green,
-              ),
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.car_rental,
-                text: '车辆管理',
-                color: Colors.blue,
-              ),
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.people,
-                text: '家人管理',
-                color: Colors.purple,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.question_mark,
-                text: '问题上报',
-                color: Colors.cyan,
-              ),
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.how_to_vote,
-                text: '预算支出投票',
-                color: Colors.indigo,
-              ),
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.phone,
-                text: '联系物业',
-                color: Colors.lightGreen,
-              ),
-              ResidentIndexServiceIcon(
-                onPressed: () {},
-                icon: Icons.more_horiz,
-                text: '更多服务',
-                color: Colors.grey,
-              ),
-            ],
-          ),
-          // 卡片底部留 8 像素的空，不然会跟上面的组件贴合太近
-          const SizedBox(height: 8),
-        ],
-      ),
+    return Column(
+      children: [
+        Row(
+          children: [
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.person,
+              text: '实名认证',
+              color: Colors.orange,
+            ),
+            ResidentIndexServiceIcon(
+              onPressed: () =>
+                  navPush(context, ResidentHouseList(communityId: communityId)),
+              icon: Icons.home,
+              text: '房屋管理',
+              color: Colors.green,
+            ),
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.car_rental,
+              text: '车辆管理',
+              color: Colors.blue,
+            ),
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.people,
+              text: '家人管理',
+              color: Colors.purple,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.question_mark,
+              text: '问题上报',
+              color: Colors.cyan,
+            ),
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.how_to_vote,
+              text: '预算支出投票',
+              color: Colors.indigo,
+            ),
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.phone,
+              text: '联系物业',
+              color: Colors.lightGreen,
+            ),
+            ResidentIndexServiceIcon(
+              onPressed: () {},
+              icon: Icons.more_horiz,
+              text: '更多服务',
+              color: Colors.grey,
+            ),
+          ],
+        ),
+        // 卡片底部留 8 像素的空，不然会跟上面的组件贴合太近
+        const SizedBox(height: 8),
+      ],
     );
   }
 }
@@ -199,40 +195,37 @@ class ResidentIndexNews extends StatelessWidget {
     // 参见 https://stackoverflow.com/questions/45669202/how-to-add-a-listview-to-a-column-in-flutter
     // ListView 在 Column 中需要有确定的高度
     return Expanded(
-      child: Card(
-        child: Column(
-          children: [
-            ListTile(
-              leading: const Icon(Icons.newspaper),
-              title: Row(
-                children: [
-                  const Text('通知公告'),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: const Text('更多'),
-                      ),
+      child: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.newspaper),
+            title: Row(
+              children: [
+                const Text('通知公告'),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text('更多'),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
-            const Divider(),
-            Expanded(
-              child: ListView.builder(
-                itemCount: notifications.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(notifications[index].getStringValue('title')),
-                    subtitle: Text(notifications[index].updated.split(' ')[0]),
-                  );
-                },
-              ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: notifications.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(notifications[index].getStringValue('title')),
+                  subtitle: Text(notifications[index].updated.split(' ')[0]),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
